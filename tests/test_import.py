dfdeb67b45b34a2628c7511d28c24621fd8344bb
Philip Rehberger <me@philiprehberger.com>
2026-03-16 16:09:34 -0500

    Release v0.1.5

---
+++ tests/test_import.py
@@ -0,0 +1,7 @@
+"""Basic import test."""
+
+
+def test_import():
+    """Verify the package can be imported."""
+    import philiprehberger_humanize_bytes
+    assert hasattr(philiprehberger_humanize_bytes, "__name__") or True
